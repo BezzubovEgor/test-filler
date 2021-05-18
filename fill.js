0093b8a598c6087b8fece86cad6dccfa00aae685
Yahor Bezzubau <yahor.bezzubau@EUNL01POLP01879.local>
2021-05-19 02:59:45 +0300

    add ling to data in repo

--- fill.js
+++ fill.js
@@ -1,23 +1,19 @@
-const Q_REG = /['"«»`]/g;
-
-function format(str) {
-    return str.trim().replace(/['"«»`]/g, '');
-}
-
-document
-    .querySelectorAll('.que > .content > .qtext')
-    .forEach(item => {
-        const itemText = item.textContent.trim().replace(/['"«»`]/g, '');
-        const question = data.find(({ question }) => question.includes(itemText));
-        if (question) {
-            item.parentNode.querySelectorAll('label').forEach(label => {
-                const labelText = label.textContent.trim().replace(/['"«»`]/g, '');
-                console.log(labelText, question.answers)
-                if (question.answers.find(answer => answer.includes(labelText))) {
-                    label.click();
+fetch('https://raw.githubusercontent.com/BezzubovEgor/test-filler/main/dist/result.json')
+    .then(data => data.json())
+    .then((data) => document
+        .querySelectorAll('.que > .content > .qtext')
+            .forEach(item => {
+                const itemText = item.textContent.trim().replace(/['"«»`]/g, '');
+                const question = data.find(({ question }) => question.includes(itemText));
+                if (question) {
+                    item.parentNode.querySelectorAll('label').forEach(label => {
+                        const labelText = label.textContent.trim().replace(/['"«»`]/g, '');
+                        if (question.answers.find(answer => answer.includes(labelText))) {
+                            label.click();
+                        }
+                    });
+                } else {
+                    console.log(item.textContent.trim())
                 }
-            });
-        } else {
-            console.log(item.textContent.trim())
-        }
-    })
+            })
+    );
