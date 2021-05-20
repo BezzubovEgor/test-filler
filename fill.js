c03493bcac1c7bc42aceda0790c58b8f1863008d
Yahor Bezzubau <yahor.bezzubau@EUNL01POLP01879.local>
2021-05-21 02:55:32 +0300

    improve fill

--- fill.js
+++ fill.js
@@ -11,19 +11,24 @@ fetch(
         return document
             .querySelectorAll(".que > .content > .qtext")
             .forEach(function (item) {
-                var itemText = item.textContent.trim().replace(/['"«»`]/g, "");
+                var itemText = item.textContent.trim().replace(/['"«»`:,.-\?\!]/g, "");
                 var question = data.find(function (_ref) {
                     var question = _ref.question;
-                    return question.includes(itemText);
+                    return question.toLowerCase().includes(itemText.toLowerCase());
+                }) || data.find(function (_ref) {
+                    var question = _ref.question;
+                    return itemText.toLowerCase().includes(question.toLowerCase());
                 });
 
                 if (question) {
                     item.parentNode.querySelectorAll("label").forEach(function (label) {
-                        var labelText = label.textContent.trim().replace(/['"«»`]/g, "");
+                        var labelText = label.textContent.trim().replace(/['"«»`:,.-\?\!]/g, "");
 
                         if (
                             question.answers.find(function (answer) {
-                                return answer.includes(labelText);
+                                return answer.toLowerCase().includes(labelText.toLowerCase());
+                            }) || question.answers.find(function (answer) {
+                                return labelText.toLowerCase().includes(answer.toLowerCase());
                             })
                         ) {
                             label.click();
