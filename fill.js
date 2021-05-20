4851581c08fc5bb202af60d74adec1359b33e22f
Yahor Bezzubau <yahor.bezzubau@EUNL01POLP01879.local>
2021-05-20 10:40:58 +0300

    old  browsers variant

--- fill.js
+++ fill.js
@@ -1,19 +1,37 @@
-fetch('https://raw.githubusercontent.com/BezzubovEgor/test-filler/main/dist/result.json')
-    .then(data => data.json())
-    .then((data) => document
-        .querySelectorAll('.que > .content > .qtext')
-            .forEach(item => {
-                const itemText = item.textContent.trim().replace(/['"«»`]/g, '');
-                const question = data.find(({ question }) => question.includes(itemText));
+"use strict";
+
+fetch(
+    "https://raw.githubusercontent.com/BezzubovEgor/test-filler/main/dist/result.json"
+)
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (data) {
+        console.log('amount of loaded questions:', data.length);
+        return document
+            .querySelectorAll(".que > .content > .qtext")
+            .forEach(function (item) {
+                var itemText = item.textContent.trim().replace(/['"«»`]/g, "");
+                var question = data.find(function (_ref) {
+                    var question = _ref.question;
+                    return question.includes(itemText);
+                });
+
                 if (question) {
-                    item.parentNode.querySelectorAll('label').forEach(label => {
-                        const labelText = label.textContent.trim().replace(/['"«»`]/g, '');
-                        if (question.answers.find(answer => answer.includes(labelText))) {
+                    item.parentNode.querySelectorAll("label").forEach(function (label) {
+                        var labelText = label.textContent.trim().replace(/['"«»`]/g, "");
+
+                        if (
+                            question.answers.find(function (answer) {
+                                return answer.includes(labelText);
+                            })
+                        ) {
                             label.click();
                         }
                     });
                 } else {
-                    console.log(item.textContent.trim())
+                    console.log(item.textContent.trim());
                 }
-            })
-    );
+            });
+    })
+    .then(() => console.log('success'));
